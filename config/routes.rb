38b2326c9194d4acd6989fcf8609d712153983db
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-27 20:10:45 +0200

    Fixed unique usernames, starting on profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "webrts#index"
+
+  get '/profile/:username', to: 'webrts#profile', as: 'patient'
+  get '/profile', to: 'webrts#profile'
 end
