50751b60971655846f56d82941c85a45dc8df7c1
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-28 21:56:09 +0200

    Moving the map creation to server side again

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,5 @@
 class MapsController < ApplicationController
+  def map
+    @map = getMapData(params[:x],params[:y],params[:size])
+  end
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,18 @@
 class Map < ApplicationRecord
   belongs_to :town, optional:true
   validates :x, uniqueness: {scope: :y}
+
+  def getMapData(xc,yc,size)
+    map = Map.all
+    rendered_tiles = []
+    for xval in (xc-(size/2))...(xc+(size/2))
+      for yval in (yc-(size/2))...(yc+(size/2))
+        rendered_tiles[xval][yval] = map.find {|z| z[:x] == xval && z[:y] == yval}
+      end
+    end
+    if !rendered_tiles[xc][yc].empty?
+      return rendered_tiles
+    end
+  end
+
 end
