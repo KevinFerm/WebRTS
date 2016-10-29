eab90da8e857fd13f41d69343d367a14ff40ded0
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-29 22:32:55 +0200

    Adding models and migrations for building and movements of troops

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029152231) do
+ActiveRecord::Schema.define(version: 20161029201748) do
 
   create_table "maps", force: :cascade do |t|
     t.integer  "x"
@@ -30,22 +30,23 @@ ActiveRecord::Schema.define(version: 20161029152231) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "username"
-    t.integer  "permissions",            default: 0,  null: false
+    t.integer  "permissions",            default: 0,     null: false
     t.integer  "points"
     t.string   "rank"
+    t.integer  "coins",                  default: 10000, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username"
