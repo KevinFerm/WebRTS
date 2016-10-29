7c3b6ccb3817eb5ccb8cf0560871afdcb33301f1
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-29 22:15:36 +0200

    Configuring town view

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/profile/:username', to: 'webrts#profile', as: 'profile'
   get '/profile', to: 'webrts#profile'
   post '/getmapdata', to: 'maps#getMapData'
-  get '/town/:id', to 'towns#town', as: 'town'
+  get '/town/:id', to: 'towns#town', as: 'town'
 end
