a2a36919baa6fdee54bd2fecc54c4b6d54c3919b
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-29 22:59:03 +0200

    Finish up the basic migrations

--- db/migrate/20161029203149_create_buildings.rb
+++ db/migrate/20161029203149_create_buildings.rb
@@ -4,7 +4,8 @@ class CreateBuildings < ActiveRecord::Migration[5.0]
       t.string :type, null: false, default: ""
       t.integer :amount, null: false, default: 0
       t.integer :done_at, null: false, default: 0
-      t.integer :town_id
+      t.integer :town_id, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
--- db/migrate/20161029203224_create_movements.rb
+++ db/migrate/20161029203224_create_movements.rb
@@ -4,6 +4,10 @@ class CreateMovements < ActiveRecord::Migration[5.0]
       t.boolean :type, null: false, default: 0
       t.integer :bowmen, null: false, default: 0
       t.integer :swordsmen, null: false, default: 0
+      t.integer :from_town, null: false, default: 0
+      t.integer :to_town, null: false, default: 0
+      t.integer :from, null: false
+      t.integer :to, null: false
       t.integer :done_at, null: false, default: 0
 
 
