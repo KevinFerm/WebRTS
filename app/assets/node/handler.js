962a715f41aa70642656543983907baef67aede5
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-31 00:35:22 +0100

    fix typo

--- app/assets/node/handler.js
+++ app/assets/node/handler.js
@@ -3,12 +3,12 @@ var http = require('http');
 function buildHandler() {
     http.get('http://localhost:3000/buildhandler');
 }
-function movedHandler() {
+function moveHandler() {
     http.get('http://localhost:3000/movehandler');
 }
 
 setInterval(function () {
-    buildHandler()
-    moveHandler()
-    Console.log("Ran")
+    buildHandler();
+    moveHandler();
+    Console.log("Ran");
 }, 10000);
