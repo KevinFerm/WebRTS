042e0a6fd8ca0cbdad3a202b126157dc3c1ea029
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-31 00:34:57 +0100

    Added movehandler to node

--- app/assets/node/handler.js
+++ app/assets/node/handler.js
@@ -3,8 +3,12 @@ var http = require('http');
 function buildHandler() {
     http.get('http://localhost:3000/buildhandler');
 }
+function movedHandler() {
+    http.get('http://localhost:3000/movehandler');
+}
 
 setInterval(function () {
-    buildHandler();
-    console.log("Running buildhandler--")
+    buildHandler()
+    moveHandler()
+    Console.log("Ran")
 }, 10000);
