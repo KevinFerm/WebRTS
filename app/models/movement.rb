2c5bd6d149f784dca0dd47519795dcfeec89a606
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-10-30 15:09:49 +0100

    Added some validation and removing troops before sending them

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,23 +1,28 @@
 class Movement < ApplicationRecord
   belongs_to :user, :foreign_key => "from"
 
-  ##This function and the ones I use now make a total of four queries, really should think about another way to do this
+  ##This function and the ones I use now make a total of five queries, really should think about another way to do this
   def self.beginMovement(move_type, bowmen, swordsmen, from_town, to_town, from, to)
     distance = Town.getTownDistance(from_town, to_town)
     time = Time.now
     done_at = time + distance.minutes
-    from_town = Town.find(from_town)
-    if Movement.create(move_type: move_type,
-                       bowmen: bowmen,
-                       swordsmen: swordsmen,
-                       from_town: from_town,
-                       to_town: to_town,
-                       from: from,
-                       to: to,
-                       done_at: done_at.to_i)
-      return true
-    else
-      return false
+    town = Town.find(from_town)
+    if(town.swordsmen >= swordsmen && town.bowmen >= bowmen)
+      if Movement.create(move_type: move_type,
+                         bowmen: bowmen,
+                         swordsmen: swordsmen,
+                         from_town: from_town,
+                         to_town: to_town,
+                         from: from,
+                         to: to,
+                         done_at: done_at.to_i)
+        town.swordsmen = (town.swordsmen-swordsmen)
+        town.bowmen = (town.bowmen-bowmen)
+        town.save
+        return true
+      else
+        return false
+      end
     end
 
   end
